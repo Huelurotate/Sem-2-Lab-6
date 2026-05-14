7a43f5e76602a934c8bfd0cda7122115d2132319
Vlad Yarovikov <protanomicon@gmail.com>
2026-05-14 22:16:58 +0300

    Made 4th queue assemble using data from the other 3

--- Zad3/functions.c
+++ Zad3/functions.c
@@ -16,23 +16,27 @@ void menu(Node** tail1, Node** tail2, Node** tail3, Node** tail4)
 		switch (menu_choice)
 		{
 		case 1:
-			input_queues(tail1, tail2, tail3, tail4, &length);
+			input_three_queues(tail1, tail2, tail3, tail4, &length);
 			break;
 		case 2:
 			if (is_empty(*tail1) || is_empty(*tail2) || is_empty(*tail3))
 				puts("\nQueue(s) is empty.");
 			else
-				output_queues(*tail1, *tail2, *tail3, *tail4);
+				output_three_queues(*tail1, *tail2, *tail3);
 			break;
 		case 3:
 			if (is_empty(*tail1) || is_empty(*tail2) || is_empty(*tail3))
-				puts("\nQueue(s) is empty.");
-			else if (!is_empty(*tail4))
-				puts("\nYou have already filled the 4th queue.");
+				puts("\nYou need to fill first 3 queues.");
 			else
-				input_4th_queue(tail4, length);
+				input_4th_queue(*tail1, *tail2, *tail3, tail4, length);
 			break;
 		case 4:
+			if (is_empty(*tail4))
+				puts("\n4th queue is empty.");
+			else
+				output_4th_queue(*tail4);
+			break;
+		case 5:
 			is_running = 0;
 			break;
 		}
@@ -44,9 +48,10 @@ void print_menu()
 	puts("\n      MENU\n"
 		"Choose an option:\n"
 		"1 - Input Queues\n"
-		"2 - Print Queues\n"
+		"2 - Print 3 Queues\n"
 		"3 - Input 4th Queue\n"
-		"4 - Exit Program");
+		"4 - Print 4th Queue\n"
+		"5 - Exit Program");
 }
 
 void menu_option_choice(int* choice)
@@ -54,14 +59,14 @@ void menu_option_choice(int* choice)
 	while (1)
 	{
 		int_input(choice);
-		if (*choice < 1 || *choice > 4)
-			puts("\nChoose an option(1-4).");
+		if (*choice < 1 || *choice > 5)
+			puts("\nChoose an option(1-5).");
 		else
 			break;
 	}
 }
 
-void input_queues(Node** tail1, Node** tail2, Node** tail3, Node** tail4, int* length)
+void input_three_queues(Node** tail1, Node** tail2, Node** tail3, Node** tail4, int* length)
 {
 	if (!is_empty(*tail1) || !is_empty(*tail2) || !is_empty(*tail3) || !is_empty(*tail4))
 	{
@@ -160,41 +165,71 @@ void int_input(int* var)
 	}
 }
 
-void output_queues(Node* tail1, Node* tail2, Node* tail3, Node* tail4)
+void output_three_queues(Node* tail1, Node* tail2, Node* tail3)
 {
 	Node* temp1 = tail1->next;
 	Node* temp2 = tail2->next;
 	Node* temp3 = tail3->next;
-	Node* temp4 = (!is_empty(tail4)) ? tail4->next : NULL;
 
-	printf("\n NAME:            MIDDLE NAME:     SURNAME:         ");
-	if (!is_empty(tail4))
-		printf("CITY:");
-	puts("");
+	puts("\n NAME:            MIDDLE NAME:     SURNAME:");
 
 	do
 	{
-		printf("|%-16s|%-16s|%-16s|", temp1->data, temp2->data, temp3->data);
-		if (!is_empty(tail4))
-			printf("%-16s|", temp4->data);
-		puts("");
+		printf("|%-16s|%-16s|%-16s|\n", temp1->data, temp2->data, temp3->data);
 		temp1 = temp1->next;
 		temp2 = temp2->next;
 		temp3 = temp3->next;
-		if(!is_empty(tail4)) temp4 = temp4->next;
 	} while (temp1 != tail1->next);
-
-	puts("");
 }
 
-void input_4th_queue(Node** tail, int length)
+void input_4th_queue(Node* tail1, Node* tail2, Node* tail3, Node** tail4, int len)
 {
-	char* temp = NULL;
-	for(int i = 0; i < length; i++)
+	char* current_city = NULL;
+	Node* temp1 = tail1->next;
+	Node* temp2 = tail2->next;
+	Node* temp3 = tail3->next;
+
+	for (int i = 0; i < len; i++)
 	{
-		printf("\nEnter city number %d:\n", i + 1);
-		input_single_queue_value(&temp, tail);
+		printf("\nEnter city for %s %s %s: \n", temp1->data, temp2->data, temp3->data);
+		str_input(&current_city);
+
+		add_4th_queue_node(tail4, temp1->data, temp2->data, temp3->data, current_city);
+
+		temp1 = temp1->next;
+		temp2 = temp2->next;
+		temp3 = temp3->next;
+		current_city = NULL;
 	}
+}
+
+void add_4th_queue_node(Node** tail4, char* val1, char* val2, char* val3, char* val4)
+{
+	size_t dest_buffer =
+		strlen(val1) +
+		strlen(val2) +
+		strlen(val3) +
+		strlen(val4) +
+		DEST_BUFF_ADDITIONAL_LEN;
+
+	char* temp_str = malloc(dest_buffer);
+	check_char_alloc(temp_str);
+
+	snprintf(temp_str, dest_buffer, "%s %s %s %s", val1, val2, val3, val4);
+
+	enqueue(tail4, temp_str);
+}
+
+void output_4th_queue(Node* tail4)
+{
+	Node* temp = tail4->next;
+	puts("\nQueue 4 contents:");
+
+	do
+	{
+		printf("%s\n", temp->data);
+		temp = temp->next;
+	} while (temp != tail4->next);
 }
 
 void free_all_queues(Node** tail1, Node** tail2, Node** tail3, Node** tail4)
--- Zad3/functions.h
+++ Zad3/functions.h
@@ -2,6 +2,7 @@
 
 #include "queue.h"
 
+#define DEST_BUFF_ADDITIONAL_LEN 4
 #define CAPACITY 10
 
 // MENU
@@ -9,20 +10,22 @@ void menu(Node** tail1, Node** tail2, Node** tail3, Node** tail4);
 void print_menu();
 void menu_option_choice(int* choice);
 
-// INPUT
-void input_queues(Node** tail1, Node** tail2, Node** tail3, Node** tail4, int* length);
+// INPUT THREE QUEUES
+void input_three_queues(Node** tail1, Node** tail2, Node** tail3, Node** tail4, int* length);
 void input_queue_values(Node** tail1, Node** tail2, Node** tail3, int* length);
 void input_single_queue_value(char** temp_var, Node** tail);
 void continue_request(int* choice);
+
+// INPUT 4TH QUEUE
+void input_4th_queue(Node* tail1, Node* tail2, Node* tail3, Node** tail4, int len);
+void add_4th_queue_node(Node** tail4, char* val1, char* val2, char* val3, char* val4);
 
 void str_input(char** str);
 void int_input(int* var);
 
 // OUTPUT
-void output_queues(Node* tail1, Node* tail2, Node* tail3, Node* tail4);
-
-// INPUT 4TH QUEUE
-void input_4th_queue(Node** tail, int length);
+void output_three_queues(Node* tail1, Node* tail2, Node* tail3);
+void output_4th_queue(Node* tail);
 
 // FREEING AND CHECKS
 void free_all_queues(Node** tail1, Node** tail2, Node** tail3, Node** tail4);
